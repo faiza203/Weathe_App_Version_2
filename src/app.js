9286a23f0ed95bebb30a2feede8f6701ce86ade7
faiza203 <60883181+faiza203@users.noreply.github.com>
2021-01-03 10:08:32 -0800

    Install hbs

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 8000;
 const staticPath = path.join(__dirname , "../public");
+
 app.use(express.static(staticPath));
+app.set('view engine', 'hbs');
 
 // app.get("/" , (req,res) => {
 //     res.send("Welcome")
