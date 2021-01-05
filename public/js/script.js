7f13b054162a6edd76af4eec5e996f5580cce8fb
faiza203 <60883181+faiza203@users.noreply.github.com>
2021-01-05 11:58:13 -0800

    Add day and date

--- public/js/script.js
+++ public/js/script.js
@@ -2,10 +2,20 @@ const body = document.getElementsByTagName("body");
 const btn = document.getElementById("submit-button");
 const city = document.getElementById("weather-city");
 const message = document.getElementById("city-name")
+const day = document.getElementById("day");
+const date = document.getElementById("date");
 
 const id = body[0].id;
-
-btn.addEventListener("click", async (e)=> {
+const currentDate = new Date();
+const days = [
+    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
+];
+const month = [
+    "Jan", "Feb", "March", "April", "May", "June", "July", "August", "Sep", "Oct", "Nov", "Dec",
+];
+day.innerText = days[currentDate.getDay() - 1]
+date.innerText = `${currentDate.getDate()} ${month[currentDate.getMonth()]}`
+btn.addEventListener("click", async (e) => {
     e.preventDefault();
     if (city.value.trim() !== "") {
         try {
