f2021da7ba7ff717994682faebcad101c1f55308
faiza203 <60883181+faiza203@users.noreply.github.com>
2021-01-05 11:46:35 -0800

    Fecth Data correctly

--- public/js/script.js
+++ public/js/script.js
@@ -2,11 +2,22 @@ const body = document.getElementsByTagName("body");
 const btn = document.getElementById("submit-button");
 const city = document.getElementById("weather-city");
 const message = document.getElementById("city-name")
+
 const id = body[0].id;
 
-btn.addEventListener("click", (e) => {
+btn.addEventListener("click", async (e)=> {
     e.preventDefault();
-    if (city.value.trim() === "") {
-        message.innerText = "Please! Add City Before Search"
+    if (city.value.trim() !== "") {
+        try {
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${id}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            // console.log(data);
+            console.log(data);
+        } catch {
+            message.innerText = "Add City Name Properly ";
+        }
+    } else {
+        message.innerText = "Add City Name Before Search";
     }
 })
