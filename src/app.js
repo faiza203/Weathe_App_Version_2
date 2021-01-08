03e82b53d0f5c9c8e10c80eacd111ce6fe80cf35
faiza203 <60883181+faiza203@users.noreply.github.com>
2021-01-07 18:50:54 -0800

    Adding ports and start script

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8000;
 const staticPath = path.join(__dirname, "../public");
 const hbsPath = path.join(__dirname, "../views");
 require("dotenv").config({
-    path: __dirname + "\\.env",
+    path: __dirname + "./../.env",
 });
 
 app.use(express.static(staticPath));
@@ -22,10 +22,7 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.render("about")
 })
-function replaceId(app , value){
-    const result = app.replace(value);
-    return result
-}
+
 app.get("/weather", (req, res) => {
     res.render("weather" , {
          apiId : process.env["WEATHER_API_TOKEN"]
